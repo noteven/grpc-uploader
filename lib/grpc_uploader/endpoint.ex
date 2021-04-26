8b65a882766d7e29681cdb94315fb18e6d35b369
noteven <15650992+noteven@users.noreply.github.com>
2021-04-26 09:12:02 -0600

    Added in basic endpoint. This should now provide the bare minimum for the gRPC server to run without faulting out.

--- lib/grpc_uploader/endpoint.ex
+++ lib/grpc_uploader/endpoint.ex
@@ -1,8 +1,7 @@
+# Define your endpoint
+defmodule GRPCUploader.Endpoint do
+  use GRPC.Endpoint
 
-  # Define your endpoint
-  defmodule GRPCUploader.Endpoint do
-    use GRPC.Endpoint
-
-    intercept(GRPC.Logger.Server)
-    run(GRPCUploader.API.Server)
-  end
+  intercept(GRPC.Logger.Server)
+  run(GRPCUploader.API.Server)
+end
